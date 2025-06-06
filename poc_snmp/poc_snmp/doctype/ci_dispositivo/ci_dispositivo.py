3232584c54fba416290f0aacd7fbe37825b99ecd
David162893 <davidrolo2005@gmail.com>
2025-06-06 11:23:55 +0200

    V6

--- poc_snmp/poc_snmp/doctype/ci_dispositivo/ci_dispositivo.py
+++ poc_snmp/poc_snmp/doctype/ci_dispositivo/ci_dispositivo.py
@@ -3,10 +3,18 @@
 
 import frappe
 from frappe.model.document import Document
-
+from frappe.model.mapper import get_mapped_doc
 
 class CIDispositivo(Document):
-	pass
+	def validate(self):
+		if self.plantilla_mib and not self._doc_before_save:
+			plantilla = frappe.get_doc("CI Plantilla Comprobacion", self.plantilla_mib)
+			for comprobacion in plantilla.ci_comprobacion:
+				self.append("ci_comprobacion", {
+					"denominacion": comprobacion.denominacion,
+					"frecuencia": comprobacion.frecuencia,
+					"oid": comprobacion.oid
+				})
 
 @frappe.whitelist()
 def delete_last_sent(docname):
