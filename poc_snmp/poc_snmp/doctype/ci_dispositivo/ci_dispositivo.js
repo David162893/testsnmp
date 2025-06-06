1e84db9e5b339bed9252d187f378abb99fe274a3
David162893 <davidrolo2005@gmail.com>
2025-06-06 12:26:48 +0200

    V7

--- poc_snmp/poc_snmp/doctype/ci_dispositivo/ci_dispositivo.js
+++ poc_snmp/poc_snmp/doctype/ci_dispositivo/ci_dispositivo.js
@@ -1,18 +1,64 @@
 // Copyright (c) 2025, David and contributors
 // For license information, please see license.txt
 
+
+// Controlador principal del formulario CI Dispositivo
+// Maneja eventos y lógica personalizada/
 frappe.ui.form.on("CI Dispositivo", {
-    refresh: function(frm) {
-        frm.add_custom_button(__('Reenviar Registro'), function() {
+
+    
+    // Evento refresh - Se ejecuta al cargar/actualizar el formulario
+    refresh: function (frm) {
+        // Agrega botón personalizado para reenvío de registros
+        frm.add_custom_button(__('Reenviar Registro'), function () {
             frappe.call({
                 method: "poc_snmp.poc_snmp.doctype.ci_dispositivo.ci_dispositivo.delete_last_sent",
                 args: {
-                    docname: frm.docname
+                    docname: frm.docname // Envía el nombre del documento actual
                 },
-                callback: function(r) {
+                callback: function (r) {
                     frm.reload_doc();
                 }
             });
         });
+    },
+
+    // Evento plantilla_mib - Se dispara al cambiar el campo plantilla_mib
+    plantilla_mib: function (frm) {
+        // Caso 1: Hay plantilla seleccionada
+        if (frm.doc.plantilla_mib) {
+            frappe.call({
+                method: 'frappe.client.get',
+                args: {
+                    doctype: 'CI Plantilla comprobacion',
+                    name: frm.doc.plantilla_mib
+                },
+                callback: function (r) {
+                    // Si la plantilla tiene comprobaciones
+                    if (r.message && r.message.comprobaciones) {
+                        // Limpia la tabla existente
+                        frm.clear_table('comprobacion');
+
+                        // Itera y añade cada comprobación
+                        r.message.comprobaciones.forEach(function (comprobacion) {
+                            let row = frm.add_child('comprobacion');
+                            row.denominacion = comprobacion.denominacion;
+                            row.frecuencia = comprobacion.frecuencia;
+                            row.oid = comprobacion.oid;
+                        });
+
+                        frm.refresh_field('comprobacion');
+                    }
+                }
+            });
+        }
+        // Caso 2: Se eliminó la plantilla
+        else {
+            // Limpia todas las comprobaciones
+            frm.clear_table('comprobacion');
+            frm.refresh_field('comprobacion');
+
+            frappe.show_alert(__("Plantilla desvinculada - Comprobaciones eliminadas"), 3);
+        }
     }
 });
--- poc_snmp/poc_snmp/doctype/ci_dispositivo/ci_dispositivo.py
+++ poc_snmp/poc_snmp/doctype/ci_dispositivo/ci_dispositivo.py
@@ -6,15 +6,7 @@ from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
 
 class CIDispositivo(Document):
-	def validate(self):
-		if self.plantilla_mib and not self._doc_before_save:
-			plantilla = frappe.get_doc("CI Plantilla Comprobacion", self.plantilla_mib)
-			for comprobacion in plantilla.ci_comprobacion:
-				self.append("ci_comprobacion", {
-					"denominacion": comprobacion.denominacion,
-					"frecuencia": comprobacion.frecuencia,
-					"oid": comprobacion.oid
-				})
+	pass
 
 @frappe.whitelist()
 def delete_last_sent(docname):
